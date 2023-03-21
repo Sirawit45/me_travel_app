88ccc2a01837d4b4d390d79c74f83c232fc3c4a3
Sirawit45 <saveso0607@gmail.com>
2023-03-21 15:09:10 +0700

    Code V.1.1 by Sirawit : create File for screen , add image and icom..........,

---
+++ lib/views/spalash_screen_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpalashScreenUI extends StatefulWidget {
+  const SpalashScreenUI({super.key});
+
+  @override
+  State<SpalashScreenUI> createState() => _SpalashScreenUIState();
+}
+
+class _SpalashScreenUIState extends State<SpalashScreenUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold() ;
+  }
+}
