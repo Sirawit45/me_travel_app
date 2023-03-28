25f5df4e7644e4f9d613715d530a17413dccc905
Sirawit45 <saveso0607@gmail.com>
2023-03-28 16:47:18 +0700

    Code V.1.3 add  login screen register screen and spalash screen

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:me_travel_app/views/regiter_ui.dart';
 
 class LoginUI extends StatefulWidget {
   const LoginUI({super.key});
@@ -10,6 +14,148 @@ class LoginUI extends StatefulWidget {
 class _LoginUIState extends State<LoginUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold() ;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[300],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.15,
+              ),
+              Image.asset(
+                'assets/images/travel.png',
+                height: MediaQuery.of(context).size.width * 0.4,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.06,
+              ),
+              Text(
+                'เข้าใช้งานแอปพลิเคชั่น',
+                style: GoogleFonts.kanit(
+                    fontSize: MediaQuery.of(context).size.width * 0.06,
+                    color: Colors.blueAccent),
+              ),
+              Text(
+                'บันทึกการเดินทาง',
+                style: GoogleFonts.kanit(
+                  fontSize: MediaQuery.of(context).size.width * 0.04,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.15,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.15,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'ชื่อผู้ใช้',
+                      style: GoogleFonts.kanit(
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.15,
+                  right: MediaQuery.of(context).size.width * 0.15,
+                ),
+                child: TextField(),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.1,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.15,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'รหัสผ่าน',
+                      style: GoogleFonts.kanit(
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.15,
+                  right: MediaQuery.of(context).size.width * 0.15,
+                ),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.visibility_off,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.08,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'SIGN IN',
+                  style: GoogleFonts.kanit(),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width * 0.7,
+                    MediaQuery.of(context).size.width * 0.125,
+                  ),
+                  backgroundColor: Colors.lightBlue,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(
+                    value: false,
+                    onChanged: (val) {},
+                  ),
+                  Text(
+                    'จำค่าการเข้าใช้งานแอปพลิเคชั่น',
+                    style: GoogleFonts.kanit(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.05,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterUI(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'ลงชื่อเข้าใช้งาน',
+                  style: GoogleFonts.kanit(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/views/spalash_screen_ui.dart
+++ lib/views/spalash_screen_ui.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:me_travel_app/views/login_ui.dart';
 
-class SpalashScreenUI extends StatefulWidget {
-  const SpalashScreenUI({super.key});
+class SplashScreenUI extends StatefulWidget {
+  const SplashScreenUI({super.key});
 
   @override
-  State<SpalashScreenUI> createState() => _SpalashScreenUIState();
+  State<SplashScreenUI> createState() => _SplashScreenUIState();
 }
 
-class _SpalashScreenUIState extends State<SpalashScreenUI> {
+class _SplashScreenUIState extends State<SplashScreenUI> {
+  @override
+  void initState() {
+    Future.delayed(
+        Duration(
+          seconds: 3,
+        ), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginUI(),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    ) ;
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/travel.png',
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Text(
+              'บันทึกการเดินทาง',
+              style: GoogleFonts.kanit(
+                fontSize: MediaQuery.of(context).size.width * 0.08,
+                color: Colors.grey[800],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Text(
+              'Created by : 6319410020\nSirawit Plailahan',
+              style: GoogleFonts.kanit(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
